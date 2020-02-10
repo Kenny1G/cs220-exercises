7504b6d641d226559cd178f9f5581912884e08d7
kenny <oseleonose@gmail.com>
2020-02-10 07:41:47 -0600

    count1 exercise complete

--- exercises/ex2-2/count1.c
+++ exercises/ex2-2/count1.c
@@ -29,14 +29,14 @@ int main() {
     int rci = 0;  //index into reverse complement array
 
     // TODO: set the value of rev_comp[rci] for every valid index
-    for (int i = dna_len; i >= 0; --i)
+    for (int i = dna_len - 1; i >= 0; --i)
     {
 	    rev_comp[rci] = dna[i];
-	    rci++;
+	    ++rci;
     }
 
     // TODO: add the null character to the end of rev_comp
-    rev_comp[-1] = '\0';
+    rev_comp[dna_len] = '\0';
 
 
 
