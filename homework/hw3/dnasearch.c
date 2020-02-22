aa868eb7cec763c7aa4bc301f1fd3e4a70cc2fa6
Kenny <oseleonose@gmail.com>
2020-02-22 16:18:02 -0500

    parse_file function take 1

--- homework/hw3/dnasearch.c
+++ homework/hw3/dnasearch.c
@@ -1,2 +1,46 @@
 // name: Kenneth Oseleononmen
 // ID:   KOSELEO1
+
+
+#include "dnasearch.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char* parse_file(const char* filename)
+{
+    char* file_array = malloc(15000 * sizeof(char));
+    if (file_array == NULL)
+    {
+        printf("dnasearch::parse_file malloc for file_array failed \n");
+    }
+
+    FILE* fileptr = fopen(filename, "r");
+    if (fileptr == NULL)
+    {
+        printf("dnasearch::parse_file error could not open file, %s", filename);
+    }
+
+    char cRet;
+    int iRet = fscanf(fileptr, " %c ", &cRet);
+    while (iRet != EOF)
+    {
+        switch(cRet)
+        {
+            case 'A':
+            case 'a':
+            case 'C':
+            case 'c':
+            case 'G':
+            case 'g':
+            case 'T':
+            case 't':
+            memcpy(file_array++, &cRet, sizeof(char));
+            default:
+                printf("Invalid text \n");
+                return 2;
+        }
+        iRet = fscanf(fileptr, " %c ", ++file_array);
+    }
+
+    return file_array;
+}
--- homework/hw3/dnasearch.h
+++ homework/hw3/dnasearch.h
@@ -1,2 +1,18 @@
 // name: Kenneth Oseleononmen
 // ID:   KOSELEO1
+
+#ifndef DNASEARCH_H_
+#define DNASEARCH_H_
+
+/*
+    Return value is a character array containing every char in the file filename
+*/
+char* parse_file(const char* filename);
+
+/*
+    Return value is the first offset *greater than or equal to* start_at
+    where pattern occurs in text. Returns -1 if there is no such occurence.
+ */
+int pattern_match(const char t[], int tlen, const char p[], int plen, int start_at);
+
+#endif //DNASEARCH_H_
