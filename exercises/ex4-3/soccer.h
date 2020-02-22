fdbd9eb3792c47d39af5e5e1f2dbf42b2979429b
Kenny <oseleonose@gmail.com>
2020-02-22 11:33:27 -0500

    ex4-3 started

--- exercises/ex4-3/soccer.h
+++ exercises/ex4-3/soccer.h
@@ -4,6 +4,32 @@
 #include <stdbool.h>
 
 //TODO 1: DEFINE DATE, STAT AND PLAYER STRUCTS HERE
+//
+typedef struct
+{
+	int num_of_goals_scored;
+	int num_of_assists;
+	float pass_accuracy;
+	int min_played;
+	int num_of_shots;
+	float shot_accuracy;
+} Stat;
+
+typedef struct
+{
+	int day;
+	int month;
+	int year;
+} Date;
+
+typedef struct
+{
+	int age;
+	int jersey_number;
+	bool goalkeeper;
+	Date *date;
+	Stat *stat;
+} Player;
 
 void create_player (Player *p, bool goalie);
 void create_team(Player team[], int size);
