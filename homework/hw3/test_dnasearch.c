b326f287ad889f552e0c5d1491bfc6a3cc5f9c21
kenny <oseleonose@gmail.com>
2020-02-22 22:41:12 -0500

    extensive testing for pattern_match

--- homework/hw3/test_dnasearch.c
+++ homework/hw3/test_dnasearch.c
@@ -27,24 +27,49 @@ char* test_parse_file(const char* filename, const char* expected)
     return actual;
 }
 
-int test_pattern_match(char *test_text, int lent, char* test_pattern, int lenp)
+void test_pattern_match(char *test_text, int lent, char* test_pattern, int lenp, int expected[], int expectedl)
 {
+    int actual[expectedl];
 
-    int total_matches = 0;
+    int actual_index = 0;
     int iRet = pattern_match(test_text, lent, test_pattern, lenp, 0);
     while (iRet != -1)
     {
         printf(" %d ", iRet);
-        ++total_matches;
+        actual[actual_index] = iRet;
+        ++actual_index;
         iRet = pattern_match(test_text, lent, test_pattern, lenp, iRet + 1);
     }
-    return total_matches;
+
+    for (int i = 0; i < expectedl; ++i)
+    {
+        assert(actual[i] == expected[i]);
+    }
 }
 int main()
 {
     char *array = test_parse_file("test.txt", "CATATTACGATTACA");
+    ++array;
 
     char test1[] = {'t', 'a', 'c'};
-    assert(test_pattern_match(++array, array[0], test1, sizeof(test1)/sizeof(char)) == 2);
+    int expected[] = {5, 11};
+    test_pattern_match(array, array[0], test1, sizeof(test1)/sizeof(char), expected, 2);
+
+    char test2[] = {'a'};
+    int expected2[] = {1, 3, 6, 9, 12, 14};
+    test_pattern_match(array, array[0], test2, sizeof(test2)/sizeof(char), expected2, 6);
+
+    char test3[] = {'T','T','a'};
+    int expected3[] = {4, 10};
+    test_pattern_match(array, array[0], test3, sizeof(test3)/sizeof(char), expected3, 2);
+
+    char test4[] = {'g','C'};
+    int expected4[0] = {};
+    test_pattern_match(array, array[0], test4, sizeof(test4)/sizeof(char), expected4, 0);
+
+    free(--array);
+
+
+
 }
 
