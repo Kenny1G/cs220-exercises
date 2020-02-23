a927bfa1a1a0c18883387f0710c7f7d8212a8276
kenny <oseleonose@gmail.com>
2020-02-23 08:11:43 -0500

    transitioning computers

--- homework/hw3/hw3.c
+++ homework/hw3/hw3.c
@@ -1,2 +1,27 @@
 // Kenny Oseleononmen
 // koseleo1
+
+#include "dnasearch.h"
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    int pattern_size = 0;
+    char* user_pattern = malloc(15001 * sizeof(char));
+    char current_char;
+    user_pattern++;
+    while (scanf("%c", &current_char) != EOF)
+    {
+        switch(toupper(current_char))
+        {
+            case 'A':
+            case 'C':
+            case 'G':
+            case 'T':
+                user_pattern[]
+
+        }
+    }
+}
