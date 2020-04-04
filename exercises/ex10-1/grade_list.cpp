4db17e38509907cc9c350b9dd4b3d96761c35778
Kenny <oseleonose@gmail.com>
2020-04-04 08:22:31 -0400

    added homework 5 files from public repo

--- exercises/ex10-1/grade_list.cpp
+++ exercises/ex10-1/grade_list.cpp
@@ -1,5 +1,7 @@
 #include "grade_list.h"
 #include <cassert>
+#include <stdio.h>
+#include <string.h>
 
 // You will add function definitions to this file.
 // See TODOs in grade_list.h.
@@ -18,19 +20,39 @@ double GradeList::min() {
 
 // TODO: write a constructor (in grade_list.cpp) that has a 
 // parameter indicating the starting length of the array. 
-
+GradeList::GradeList(int capacity): grades(new double[capacity]),
+												capacity(capacity),
+												count(0)
+{}
 
 
 
 // TODO: Add the given grade to the grades list. If the array is full,
 // resize it by doubling its length. Do not use realloc!
-
+void GradeList::add(double grade)
+{
+	if (capacity >= count) {
+		capacity *= 2;
+		double *pRet = new double[capacity];
+		memcpy(pRet, grades, capacity/2);
+		delete[] grades;
+		grades = pRet;
+	}
+	else {
+		grades[count] = grade;
+	}
+}
 
 
 
 // TODO: Add the specified number of values from an array of 
 // grades to this object.
-
+void GradeList::add(int howmany, double * grades)
+{
+	for (int i = 0; i < howmany; ++i) {
+		GradeList::grades[count + i] = grades[i];
+	}
+}
 
 
 
